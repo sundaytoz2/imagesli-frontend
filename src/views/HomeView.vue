435c6452e41a574cc3eaa9ba7ce700fdf635cb13
choigawoon <choigawoon@gmail.com>
2024-03-02 20:49:18 +0900

    tailwindcss + flowbite

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
 import TheWelcome from '../components/TheWelcome.vue'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite()
+})
 </script>
 
 <template>
