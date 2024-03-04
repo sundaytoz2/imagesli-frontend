8c0393e9e0e0779cedd035c1334ea55654ea14a6
choigawoon <choigawoon@gmail.com>
2024-03-05 00:06:07 +0900

    make frontpage

---
+++ src/components/FlowerCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Flower } from '@/types';
+import { defineProps } from 'vue';
+
+const props = defineProps<Flower>();
+
+const clickedFlower = () => {
+  alert('꽃을 클릭했습니다.');
+};
+</script>
+
+<template>
+  <div @click="clickedFlower"
+    class="cursor-pointer mb-4 min-h-64 min-w-64 rounded-xl bg-yellow-200 hover:bg-yellow-300 hover:shadow">
+    <h1>{{ props.name }} </h1>
+    <p>{{ props.meaning }}</p>
+    <p>{{ props.tips }}</p>
+  </div>
+</template>
--- src/stores/flowers.ts
+++ src/stores/flowers.ts
@@ -1,13 +1,14 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import flowersData from '../mock/flowersData.json'
+import type { Flower } from '@/types'
 
 console.log(flowersData)
 
 export const useFlowerStore = defineStore('flower', () => {
   const currentMonth = ref(new Date().getMonth() + 1)
-  const flowers = ref(flowersData)
-  function getFlowerByMonth(month: number) {
+  const flowers = ref(flowersData as Flower[])
+  function getFlowerByMonth(month: number): Flower[] {
     return flowers.value.filter((flower) => flower.month === month)
   }
 
