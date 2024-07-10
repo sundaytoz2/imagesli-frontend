a2cb7590e4de96b03a29789be330e8db80a425ff
choigawoon <choigawoon@gmail.com>
2024-07-11 00:40:47 +0900

    backup

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,20 @@
 </script>
 
 <template>
+  <nav class="bg-slate-600 text-white flex justify-between items-center p-2">
+    <h1 class="ml-2 text-2xl font-bold cursor-pointer px-3 py-1 rounded bg-slate-400 hover:bg-slate-300"
+      @click="$router.push('/')">
+      Vue 3
+      Tools</h1>
+    <ul class="flex gap-x-2">
+      <li><router-link to="/tools/image-comp" class="px-3 py-1 rounded bg-slate-400 hover:bg-slate-300">Image
+          Compare</router-link></li>
+      <li><router-link to="/tools/convert" class="px-3 py-1 rounded bg-slate-400 hover:bg-slate-300">Convert JSON to
+          CSV</router-link></li>
+    </ul>
+    <div class="mr-4">help</div>
+  </nav>
   <router-view />
 </template>
+
+<style></style>
